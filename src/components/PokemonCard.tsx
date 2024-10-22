f22166666088519a42e856cd360131dbedda6d19
jottiana <11.paradise.avenue@gmail.com>
2024-10-22 19:31:25 +0200

    modified pokemon card

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,30 +1,26 @@
-
 const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/151.png",
-  },
+	{
+		name: "bulbasaur",
+		imgSrc:
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+	},
+	{
+		name: "mew",
+	},
 ];
 
 function PokemonCard() {
-  const pokemon = pokemonList[0]
-  return (
-    <figure>
-      <img src={pokemon.imgSrc} alt={`${pokemon.name}`} />        
-      <figcaption>{pokemon.name}</figcaption>
-    </figure>
-  );
-};
-
-
-
+	const pokemon = pokemonList[1];
+	return (
+		<figure>
+			{pokemon.imgSrc ? (
+				<img src={pokemon.imgSrc} alt={`${pokemon.name}`} />
+			) : (
+				<p>???</p>
+			)}
+			<figcaption>{pokemon.name}</figcaption>
+		</figure>
+	);
+}
 
 export default PokemonCard;
-
-
